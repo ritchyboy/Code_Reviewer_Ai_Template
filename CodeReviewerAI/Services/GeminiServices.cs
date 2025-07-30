5a4e36fab30a6306d76bd6cf335e9c8339f4def8
ritchyboy <ritchyboy1@hotmail.com>
2025-07-30 17:41:53 -0400

    [005]The start of contract need research about the api link

---
+++ CodeReviewerAI/Services/GeminiServices.cs
@@ -0,0 +1,26 @@
+﻿using CodeReviewerAI.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReviewerAI.Services
+{
+    public class GeminiServices : IGeminiServices
+    {
+        public string url = "";
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GeminiServices(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public async Task GetFileReviewAsync(string fileName)
+        {
+            
+        }
+    }
+}
---
+++ CodeReviewerAI/Services/IServices/IGeminiServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReviewerAI.Services.IServices
+{
+    public interface IGeminiServices
+    {
+        public Task GetFileReviewAsync(string fileName);
+    }
+}
