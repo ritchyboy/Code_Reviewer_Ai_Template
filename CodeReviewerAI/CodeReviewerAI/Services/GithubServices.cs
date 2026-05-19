fca86f30eb71ed062fb841e7f4737b3fd1d54ccf
ritchyboy <ritchyboy1@hotmail.com>
2026-05-19 02:32:46 -0400

    for .sln and .csproj too ............

--- CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
+++ CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
@@ -63,7 +63,7 @@ namespace CodeReviewerAI.Services
         private bool IsBinary(string filename)
         {
             string[] binaryExtensions = { ".png", ".jpg", ".jpeg", ".dll", ".exe", ".pdb",".yml",".gitignore"
-            ,".md"};
+            ,".md",".sln",".csproj"};
             return binaryExtensions.Any(ext => filename.EndsWith(ext, StringComparison.OrdinalIgnoreCase));
         }
 
