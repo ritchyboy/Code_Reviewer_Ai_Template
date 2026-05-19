48623c872b1b30f855066f90df40ab66df97ccd9
ritchyboy <ritchyboy1@hotmail.com>
2026-05-19 02:30:56 -0400

    for .md too .....

--- CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
+++ CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
@@ -62,7 +62,8 @@ namespace CodeReviewerAI.Services
         }
         private bool IsBinary(string filename)
         {
-            string[] binaryExtensions = { ".png", ".jpg", ".jpeg", ".dll", ".exe", ".pdb",".yml",".gitignore" };
+            string[] binaryExtensions = { ".png", ".jpg", ".jpeg", ".dll", ".exe", ".pdb",".yml",".gitignore"
+            ,".md"};
             return binaryExtensions.Any(ext => filename.EndsWith(ext, StringComparison.OrdinalIgnoreCase));
         }
 
