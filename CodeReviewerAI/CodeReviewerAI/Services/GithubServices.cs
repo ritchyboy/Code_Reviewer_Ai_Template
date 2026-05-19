9fcbdcf230085d24e27a585d47ba6c1171f6e5c0
ritchyboy <ritchyboy1@hotmail.com>
2026-05-19 02:24:42 -0400

    it must ignored yml file so it can run

--- CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
+++ CodeReviewerAI/CodeReviewerAI/Services/GithubServices.cs
@@ -62,7 +62,7 @@ namespace CodeReviewerAI.Services
         }
         private bool IsBinary(string filename)
         {
-            string[] binaryExtensions = { ".png", ".jpg", ".jpeg", ".dll", ".exe", ".pdb" };
+            string[] binaryExtensions = { ".png", ".jpg", ".jpeg", ".dll", ".exe", ".pdb",".yml" };
             return binaryExtensions.Any(ext => filename.EndsWith(ext, StringComparison.OrdinalIgnoreCase));
         }
 
